cec701d6f998b5824542760e1df0404366bd4bcd
Daniel Binyamin <daniel.binyamin@weka.io>
2026-03-02 15:29:54 +0200

    format

--- pkg/grafanadata/model.go
+++ pkg/grafanadata/model.go
@@ -48,13 +48,13 @@ type Dashboard struct {
 }
 
 type Panel struct {
-	ID            int        `json:"id"`
-	Datasource    Datasource `json:"datasource"`
-	Targets       []any      `json:"targets"`
-	Title         string     `json:"title"`
-	Panels        []Panel    `json:"panels"`        // for nested panels
-	Interval      string     `json:"interval"`      // minimum query interval, e.g. "1m", "5m"
-	MaxDataPoints *int       `json:"maxDataPoints"` // max data points for the panel query
+	ID            int         `json:"id"`
+	Datasource    Datasource  `json:"datasource"`
+	Targets       []any       `json:"targets"`
+	Title         string      `json:"title"`
+	Panels        []Panel     `json:"panels"`        // for nested panels
+	Interval      string      `json:"interval"`      // minimum query interval, e.g. "1m", "5m"
+	MaxDataPoints *int        `json:"maxDataPoints"` // max data points for the panel query
 	FieldConfig   FieldConfig `json:"fieldConfig"`
 }
 
